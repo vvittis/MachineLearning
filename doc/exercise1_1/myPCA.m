3b40e40362e5483deee25b28b6fbb9471f35acf3
Vasilis Vittis <vasilisvittis@gmail.com>
2019-03-27 17:21:39 +0200

    First Commit Exersice1 DONE

---
+++ doc/exercise1_1/myPCA.m
@@ -0,0 +1,32 @@
+function [U, S] = myPCA(X)
+%PCA Run principal component analysis on the dataset X
+%   [U, S, X] = myPCA(X) computes eigenvectors of the covariance matrix of X
+%   Returns the eigenvectors U, the eigenvalues (on diagonal) in S
+%
+
+% Useful values
+[m, n] = size(X);
+
+% You need to return the following variables correctly.
+U = zeros(n);
+S = zeros(n);
+
+% ====================== YOUR CODE GOES HERE ======================
+% Apply PCA by computing the eigenvectors and eigenvalues of the covariance matrix. 
+%
+
+covariancematrix1 = (1./(m-1)) *(X.'*X);
+
+[U,S]          = eig(covariancematrix1);
+eigenval       = diag(S);
+[eigenval,ind] = sort(eigenval,1,'descend'); %Sort them 
+U              = U(:,ind); %Corresponding eigenvectors
+S              = diag(eigenval);
+
+
+
+
+
+% =========================================================================
+
+end
