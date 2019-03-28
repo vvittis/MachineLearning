f5c5b81163c30219ed16e8c5b3c38ab9249faae9
Vasilis Vittis <vasilisvittis@gmail.com>
2019-03-28 17:07:30 +0200

    progress exercise 1.3

--- doc/exercise1_3/myLDA.m
+++ doc/exercise1_3/myLDA.m
@@ -24,28 +24,53 @@ function A = myLDA(Samples, Labels, NewDim)
         P(i)= sum(Labels()==(i-1))/size(Labels,1);
     end
     
-    mu(1,:) = mean(Samples(1:50,:))
+%     mu1(1,:) = mean(Samples(1:50,:))
     %Calculate the Class Mean 
-%      for i = 1:NumClasses
-%        mu(i,:) = mean(Samples());
-%     end
-	
+    
+     for i = 1:NumClasses
+         step = ((i-1)*50);
+         mu(i,:) = mean(Samples(step+1:step+50,:));
+    end
+	mu;
     %Calculate the Within Class Scatter Matrix
-% 	Sw=
-    %Calculate the Global Mean
-% 	m0=
+    Sw = zeros();
 
-  
+    for i = 1:NumClasses
+         step = ((i-1)*50);
+         P(i)* cov(Samples(step+1:step+50,:));
+         Sw = Sw + P(i)* cov(Samples(step+1:step+50,:));
+    end
+    Sw;
+    %Calculate the Global Mean
+	 for i = 1:NumFeatures
+         m0(i) = 0;
+         for j = 1:NumClasses
+             m0(i) = P(j).*( m0(i) + mu(j,i));
+         end 
+     end
+     m0
+     
     %Calculate the Between Class Scatter Matrix
-% 	Sb= 
+    Sb = zeros();
+    
+	 for i = 1:NumClasses
+         Sb1(i,:) = mu(i,:) - m0;
+         Sb = Sb  + P(i)*((mu(i,:) - m0)*(mu(i,:) - m0)');
+     end
+     Sb 
     
     %Eigen matrix EigMat=inv(Sw)*Sb
-%     EigMat = inv(Sw)*Sb;
+     EigMat = inv(Sw)*Sb;
     
     %Perform Eigendecomposition
-
+     [U,S]          = eig(EigMat);
+     eigenval       = diag(S);
+     [eigenval,ind] = sort(eigenval,1,'descend'); %Sort them 
+      U             = U(:,ind); %Corresponding eigenvectors
+      %Select the NewDim eigenvectors corresponding to the top NewDim
+      S             = diag(eigenval);
+      S             = S(:,1:NewDim) 
     
-    %Select the NewDim eigenvectors corresponding to the top NewDim
     %eigenvalues (Assuming they are NewDim<=NumClasses-1)
 	%% You need to return the following variable correctly.
 % 	A=zeros(NumFeatures,NewDim);  % Return the LDA projection vectors
